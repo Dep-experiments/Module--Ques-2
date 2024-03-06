8ac12d8e8189c5aa5654c9e4c6eaf5803c8aa508
Chamika Priyashanka <157556947+Chamik95Sha@users.noreply.github.com>
2024-03-06 10:44:03 +0530

    Delete project_e directory

--- project_e/src/lk/ijse/project_e/component/CustomerComponent.java
+++
@@ -1,5 +0,0 @@
-package lk.ijse.project_e.component;
-import lk.ijse.project_e.util.*;
-import lk.ijse.project_f.config.*;
-public class CustomerComponent {
-}
--- project_e/src/module-info.java
+++
@@ -1,4 +0,0 @@
-module lk.ijse.project_e {
-   requires transitive lk.ijse.project_f;
-   exports lk.ijse.project_e.component to lk.ijse.project_c;
-}
